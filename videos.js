f1f7092b7902832301b6ed30f7771dd92a79f346
Jared McDaniel <jmcdaniel.productions@gmail.com>
2026-04-15 23:49:47 -0700

    first commit

---
+++ videos.js
@@ -0,0 +1,38 @@
+// Put your YouTube links here.
+// Leave a field blank if that project does not have a video yet.
+// The main HTML reads this file and maps URLs into the portfolio data at runtime.
+
+window.VIDEO_LINKS = {
+  reel: '',
+  direction: {
+    'cyberpunk-2077': 'https://youtu.be/9H9ylniZ_4Q',
+    'fallout-nuka-cola': 'https://youtu.be/VxTBnNhAD3Q',
+    'doom': 'https://youtu.be/0WUnGHJXeHc',
+    'witcher': 'https://youtu.be/tr1ajPqpuDs',
+    'corsair-storefront': 'https://youtu.be/OZZsRSgG70g',
+    'frame-cut': '',
+    'direction-07': '',
+    'direction-08': '',
+    'direction-09': '',
+    'direction-10': '',
+    'direction-11': ''
+  },
+  films: {
+    'film-01': '',
+    'film-02': '',
+    'film-03': '',
+    'film-04': '',
+    'film-05': '',
+    'film-06': '',
+    'film-07': ''
+  },
+  amvs: {
+    'amv-01': 'https://youtu.be/Nf_lDTJCl2w',
+    'amv-02': 'https://youtu.be/Yj2ncYs0vGU',
+    'amv-03': '',
+    'amv-04': '',
+    'amv-05': '',
+    'amv-06': '',
+    'amv-07': ''
+  }
+};
