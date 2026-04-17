975699da6eb93a9ddd970e574b33da8f67329b56
Jared McDaniel <jmcdaniel.productions@gmail.com>
2026-04-16 21:22:53 -0700

    put the wheels on

--- videos.js
+++ videos.js
@@ -3,15 +3,14 @@
 // The main HTML reads this file and maps URLs into the portfolio data at runtime.
 
 window.VIDEO_LINKS = {
-  reel: '',
+  reel: 'https://youtu.be/9H9ylniZ_4Q',
   direction: {
     'cyberpunk-2077': 'https://youtu.be/9H9ylniZ_4Q',
     'fallout-nuka-cola': 'https://youtu.be/VxTBnNhAD3Q',
     'doom': 'https://youtu.be/0WUnGHJXeHc',
     'witcher': 'https://youtu.be/tr1ajPqpuDs',
     'corsair-storefront': 'https://youtu.be/OZZsRSgG70g',
-    'frame-cut': '',
-    'direction-07': '',
+    'frame-cut': 'https://youtu.be/nAIy3q5myjY',
     'direction-08': '',
     'direction-09': '',
     'direction-10': '',
@@ -29,9 +28,9 @@ window.VIDEO_LINKS = {
   amvs: {
     'amv-01': 'https://youtu.be/Nf_lDTJCl2w',
     'amv-02': 'https://youtu.be/Yj2ncYs0vGU',
-    'amv-03': '',
-    'amv-04': '',
-    'amv-05': '',
+    'amv-03': 'https://youtu.be/q_d2habvkEQ',
+    'amv-04': 'https://youtu.be/k-Y2XvwHPaQ',
+    'amv-05': 'https://youtu.be/f9v-8og-cGk?si=dBd957PRwpwUud3B',
     'amv-06': '',
     'amv-07': ''
   }
