d30e390ad1660842842340cf2d6dc230d1da062c
Jared McDaniel <hello@jaredmcdaniel.com>
2026-05-10 22:21:27 -0700

    Finalize curated portfolio content

--- videos.js
+++ videos.js
@@ -1,37 +1,18 @@
-// Put your YouTube links here.
-// Leave a field blank if that project does not have a video yet.
+// Public portfolio video links.
 // The main HTML reads this file and maps URLs into the portfolio data at runtime.
 
 window.VIDEO_LINKS = {
   reel: 'https://youtu.be/9H9ylniZ_4Q',
   direction: {
     'cyberpunk-2077': 'https://youtu.be/9H9ylniZ_4Q',
-    'fallout-nuka-cola': 'https://youtu.be/VxTBnNhAD3Q',
     'doom': 'https://youtu.be/0WUnGHJXeHc',
-    'witcher': 'https://youtu.be/tr1ajPqpuDs',
     'corsair-storefront': 'https://youtu.be/OZZsRSgG70g',
+    'fallout-nuka-cola': 'https://youtu.be/VxTBnNhAD3Q',
     'frame-cut': 'https://youtu.be/nAIy3q5myjY',
-    'direction-08': '',
-    'direction-09': '',
-    'direction-10': '',
-    'direction-11': ''
-  },
-  films: {
-    'film-01': '',
-    'film-02': '',
-    'film-03': '',
-    'film-04': '',
-    'film-05': '',
-    'film-06': '',
-    'film-07': ''
+    'witcher': 'https://youtu.be/tr1ajPqpuDs'
   },
   amvs: {
     'amv-01': 'https://youtu.be/Nf_lDTJCl2w',
-    'amv-02': 'https://youtu.be/Yj2ncYs0vGU',
-    'amv-03': 'https://youtu.be/q_d2habvkEQ',
-    'amv-04': 'https://youtu.be/k-Y2XvwHPaQ',
-    'amv-05': 'https://youtu.be/f9v-8og-cGk?si=dBd957PRwpwUud3B',
-    'amv-06': '',
-    'amv-07': ''
+    'amv-02': 'https://youtu.be/Yj2ncYs0vGU'
   }
 };
